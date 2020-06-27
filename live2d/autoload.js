b6b46c5b80b3f71582fb56ae6d545fcf62649d17
radium-bit <1565982518@qq.com>
2020-06-27 10:45:45 +0800

    Update autoload.js

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/radium-bit/res@master/live2d/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/radium-bit/res@latest/live2d/";
 // const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
 
